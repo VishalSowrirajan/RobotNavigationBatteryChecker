45e9be61512523804c8d4f54e083a9f56be39480
Vishal Sowrirajan <vishalsowrirajan5@gmail.com>
2020-10-29 16:30:03 +0100

    master: Fix Bug

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from classFiles.DataParser import DataParser
 from classFiles.Path import Path
 from classFiles.RobotManager import RobotManager
-from utils.Constants import *
-
-from utils.Constants import path
 
 
-def main():
+def main(path, status, consumption_rate):
     # Create Object of Path class
     dataReader = Path(path)
     # Read the File
@@ -14,7 +11,7 @@ def main():
     # Process the File - Extract and Clean the coordinate information
     file_content = DataParser.getDataFromFile(read_file)
     # Robot Navigator
-    robotManager = RobotManager(CURRENT_BATTERY_STATUS, battery_consumption_per_unit_distance)
+    robotManager = RobotManager(status, consumption_rate)
     # The below method is used so that the method moveRobot can be reused for a similar data.
     distance_metric = robotManager.moveRobot(file_content)
     # If the method is successful, the robot has completed its task.
@@ -22,4 +19,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    path = "https://pastebin.com/raw/mLc80Pen"
+    # Assumption: The given coordinates are in Euclidean Space
+    battery_consumption_per_unit_distance = 10  # For this unit distance, the robot consumes 1% of the battery.
+    current_battery_status = 100  # Current Battery in percentage
+    main(path, battery_consumption_per_unit_distance, current_battery_status)
