4438e05b733bf89e7065a58400c6a6f207669282
VishalSowrirajan <59771754+VishalSowrirajan@users.noreply.github.com>
2021-02-08 16:28:51 +0100

    Update Constants.py

--- utils/Constants.py
+++ utils/Constants.py
@@ -1,4 +1,4 @@
-path = "https://pastebin.com/raw/mLc80Pen"
+path = '' # Please provide the path here
 # Assumption: The given coordinates are in Euclidean Space
 battery_consumption_per_unit_distance = 10  # For this unit distance, the robot consumes 1% of the battery.
 CURRENT_BATTERY_STATUS = 100  # Current Battery in percentage
